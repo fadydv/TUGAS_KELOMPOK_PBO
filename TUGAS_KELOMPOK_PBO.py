2bb8974e125760237c0d96dbd7424ce27559861e
faady <142226344+fadydv@users.noreply.github.com>
2026-05-11 10:22:37 +0800

    pbo

--- TUGAS_KELOMPOK_PBO.py
+++ TUGAS_KELOMPOK_PBO.py
@@ -43,3 +43,49 @@ class NotifikasiEmail:
 class RepositoriBuku:
     def simpan(self, buku: Buku):
         pass  # SQL logic
+
+#ocp
+#anti-pattern
+class KalkulatorDiskon:
+    def hitung(self, harga, tipe_member):
+        if tipe_member == "reguler":
+            return harga * 0.95
+        elif tipe_member == "silver":
+            return harga * 0.90
+        elif tipe_member == "gold":
+            return harga * 0.80
+        elif tipe_member == "platinum":
+            return harga * 0.70
+        #untuk menambah member diamond harus masuk ke sini dan mengubah method lagi
+
+#pattern
+from abc import ABC, abstractmethod
+
+class StrategiDiskon(ABC):
+    @abstractmethod
+    def hitung(self, harga: float) -> float: ...
+
+class DiskonReguler(StrategiDiskon):
+    def hitung(self, harga): return harga * 0.95
+
+class DiskonSilver(StrategiDiskon):
+    def hitung(self, harga): return harga * 0.90
+
+class DiskonGold(StrategiDiskon):
+    def hitung(self, harga): return harga * 0.80
+
+# tambah member diamond hanya tniggal membuat class baru
+class DiskonDiamond(StrategiDiskon):
+    def hitung(self, harga): return harga * 0.60
+
+class KalkulatorDiskon:
+    def __init__(self, strategi: StrategiDiskon):
+        self.strategi = strategi
+    def hitung(self, harga):
+        return self.strategi.hitung(harga)
+
+#lsp
+#anti-pattern
+
+
+#pattern
